8ec0abdf4039c80391e4098d3c7971656abd3064
lycTJ <49508677+lycTJ@users.noreply.github.com>
2021-02-01 20:21:06 +0800

    add list comprehension

--- read.py
+++ read.py
@@ -4,9 +4,9 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		#count += 1
-		#if count % 1000 == 0:
-			#print(len(data))
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
 print('檔案讀取完了，總共有', len(data),'筆資料')
 
 sum_len = 0                     #每筆留言平均長度
@@ -21,4 +21,20 @@ for d in data:
 		new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
-print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言有good')
+print(good[0])
+
+
+
+good = [1 for d in data if 'good' in d]
+print(good)
+
+
+bad = ['bad' in d for d in data]
+print(bad)
